b42f5ee8600356223ec1d3a96a9ea82b8321d232
troy <hunter007nz@gmail.com>
2022-07-08 19:54:27 +1200

    react toolkit example

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,12 @@
-
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counter'
-import authReducer from './auth';
-
-
-
+import { configureStore } from "@reduxjs/toolkit"
+import counterReducer from "./counter"
+import authReducer from "./auth"
 
 const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-        auth: authReducer
-    },
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer,
+  },
 })
 
-
-
 export default store
-
-
-
